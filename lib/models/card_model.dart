7ac7c1691aff6d9c6102cfd4959eabb30ac708da
GizemGoksu <gizemgks_282@hotmail.com>
2023-01-25 21:11:33 +0300

    card model

---
+++ lib/models/card_model.dart
@@ -0,0 +1,10 @@
+class CardModel {
+  String imagePath;
+  String title;
+  String subtitle;
+  CardModel({
+    required this.imagePath,
+    required this.title,
+    required this.subtitle,
+});
+}

d287fe118de20b5175f80d94ed8ce18f490e96d7
jjasminn <jasminn247@outlook.com>
2023-01-25 16:03:56 +0300

    Font Familys and AppColors
    Font aileleri Roboto ve Inter eklendi
    Uygulama icerisinde kullanilan tum renkler base utility icerisinde bir class nesnesi oalark eklendi.

---
+++ lib/core/base/util/base_utility.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color blueChalk = Color.fromRGBO(238, 227, 255, 1);
+  //uygulamanin genelindeki lila renk
+  static const Color cornFlowerBlue = Color.fromRGBO(102, 99, 255, 1);
+  //container golgeleri
+  static const Color white = Color.fromRGBO(254, 254, 254, 1);
+  //duz beyaz
+  static const Color melrose = Color.fromRGBO(208, 188, 255, 1);
+  //acik renk butonlarin background
+  static const Color meteorite = Color.fromRGBO(57, 30, 114, 1);
+  //acik renk buton text rengi
+  static const Color butterflyBush = Color.fromRGBO(103, 80, 164, 1);
+  //koyu renk buton
+  static const Color deepCove = Color.fromRGBO(11, 7, 54, 1);
+  //Text renkleri
+  static const Color dustyGray = Color.fromRGBO(149, 149, 149, 1);
+  //navbar
+  static const Color black = Color.fromRGBO(0, 0, 0, 1);
+  static const Color coldPurple = Color.fromRGBO(185, 168, 220, 1);
+  //mesaj
+  static const Color royalBlue = Color.fromRGBO(99, 86, 229, 1);
+  //login buton
+}

83130cc151ba3e5234f65d20c41d1deace90287a
jjasminn <jasminn247@outlook.com>
2023-01-27 16:29:41 +0300

    TextStyle/ Dosya Duzeni/Component Duzeni

---
+++ lib/model_view/row_view.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+import '../models/row_model.dart';
+
+Widget row_view(RowModel rowModel) {
+  return Padding(
+    padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 4),
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        rowModel.leadingIcon!,
+        Text(rowModel.text, style: TextStyle(fontSize: 14)),
+        rowModel.trailingIcon!
+      ],
+    ),
+  );
+}

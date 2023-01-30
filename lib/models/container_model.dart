ea4bb02adeba21ce8ec3a863a14c96f774b23bd1
ilknurtulgar <ilknur001-@hotmail.com>
2023-01-30 16:29:38 +0300

    from to ilknur-dev

--- lib/models/container_model.dart
+++ lib/models/container_model.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 class ContainerModel {
   double? width; //  buton genişliği içindeki yazıların uzunluğuna göre değişebileceği için custombutton'ın row'unun mainaxissize'ını min yaptım bu sayede width almaya gerek kalmadı
   double? height; // paylaşımdaki yazılar ne kadar uzunsa container uzunlukta olsun diye height'ı required yapmadım bu yüzden custom container'da columnda mainaxissize'a mainaxisize.min verdim

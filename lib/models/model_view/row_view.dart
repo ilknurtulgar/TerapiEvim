8a0133207bbe98ff582e6b9388802db2d77b07f8
ilknurtulgar <ilknur001-@hotmail.com>
2023-01-31 22:39:45 +0300

    ilknur-dev

--- lib/models/model_view/row_view.dart
+++ lib/models/model_view/row_view.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 import '../row_model.dart';
 
-Widget RowView(RowModel rowModel) {
+Widget rowView(RowModel rowModel) {
   return Padding(
     padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 4),
     child: Row(

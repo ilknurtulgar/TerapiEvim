758aea7921437c552f0cf0322be11b3f5542aa0e
GizemGoksu <gizemgks_282@hotmail.com>
2023-02-03 01:20:13 +0300

    group information container

---
+++ lib/screen/group/component/group_information_container.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import '../../../components/buttons/custom_button.dart';
+import '../../../components/container/participant_container.dart';
+import '../../../core/base/util/base_utility.dart';
+import '../../../models/card_model.dart';
+
+class GroupInformationContainer extends StatelessWidget {
+  GroupInformationContainer(
+      {super.key,
+      required this.groupName,
+      required this.mainTherapist,
+      required this.secondTherapist,
+      required this.numberOfParticipant,
+      required this.numberOfSession});
+  final String groupName;
+  final CardModel mainTherapist;
+  final CardModel secondTherapist;
+  final int numberOfParticipant;
+  final int numberOfSession;
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      elevation: 5,
+      borderRadius: BorderRadius.circular(16),
+      child: Container( 
+        width: 342,
+        decoration: BoxDecoration(borderRadius: BorderRadius.circular(16),color: AppColors.white),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 15),
+          child: insideTheContainer(),
+        ),
+      ),
+    );
+  }
+
+  Column insideTheContainer() {
+    return Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Center(child: paddedText(const EdgeInsets.symmetric(vertical: 15),groupName,AppTextStyles.GroupTextStyle(false))),
+            therapistRow(mainTherapist, 'Grup Terapisti: ', () { print('ana psikolog') ;}, 192),
+            therapistRow(secondTherapist, 'Yardımcı Psikolog: ', () { print('yardımcı psikolog') ;}, 170),
+            Text('Katılımcı Sayısı: $numberOfParticipant',style: AppTextStyles.GroupTextStyle(true)),
+            paddedText(const EdgeInsets.only(top: 20,bottom: 10),'Seans Sayısı: $numberOfSession',AppTextStyles.GroupTextStyle(true)),
+            Align(
+              alignment: Alignment.centerRight,
+              child: CustomButton(container: AppContainers.purpleButtonContainer,onTap:(){},text: 'Katıl',)),
+            const SizedBox(height: 15,)
+          ],
+        );
+  }
+
+  Padding paddedText(EdgeInsets padding,String text,TextStyle textStyle) {
+    return Padding(
+              padding: padding,
+              child: Text(text,style: textStyle,),
+            );
+  }
+
+  Row therapistRow(CardModel therapist, String text, Function() func,double width) {
+    return Row(
+      children: [
+        Text(text,style: AppTextStyles.GroupTextStyle(true)),
+        InkWell(
+          onTap: func,
+          child: participantContainer(therapist, 50, width,)),
+      ],
+    );
+  }
+}

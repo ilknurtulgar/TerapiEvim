21443d8f902ff52a078f04c83f07c930b33cf38c
Behzod Faiziev <behzod.faiziev@gmail.com>
2023-02-10 22:41:31 +0300

    updated auth models

--- lib/service/model/common/signup/sign_up_model.g.dart
+++ lib/service/model/common/signup/sign_up_model.g.dart
@@ -7,12 +7,12 @@ part of 'sign_up_model.dart';
 // **************************************************************************
 
 SignUpModel _$SignUpModelFromJson(Map<String, dynamic> json) => SignUpModel(
-      name: json['name'] as String?,
-      birthDate: json['birthDate'] as String?,
-      gender: json['gender'] as String?,
-      email: json['email'] as String?,
-      password: json['password'] as String?,
-      phone: json['phone'] as String?,
+      name: json['name'] as String,
+      birthDate: json['birthDate'] as String,
+      gender: json['gender'] as String,
+      email: json['email'] as String,
+      password: json['password'] as String,
+      phone: json['phone'] as String,
     );
 
 Map<String, dynamic> _$SignUpModelToJson(SignUpModel instance) =>

b183a6da2ef0c31d7dd39fa351f8fd8942251901
ilknurtulgar <ilknur001-@hotmail.com>
2023-02-12 02:34:55 +0300

    update

--- lib/screen/login/participant_ui/participant_login_page.dart
+++ lib/screen/login/participant_ui/participant_login_page.dart
@@ -9,7 +9,8 @@ import '../../sign_up/participant_ui/participant_signup_page.dart';
 class ParticipantLoginPage extends StatelessWidget {
   ParticipantLoginPage({super.key});
   final TextEditingController mailControllerLoginPage = TextEditingController();
-  final TextEditingController passwordControllerLoginPage = TextEditingController();
+  final TextEditingController passwordControllerLoginPage =
+      TextEditingController();
   final TextfieldUtility textfieldUtility = TextfieldUtility();
   @override
   Widget build(BuildContext context) {
@@ -21,12 +22,25 @@ class ParticipantLoginPage extends StatelessWidget {
           children: [
             loginPageTitle(),
             textfieldUtility.mailTextfield(mailControllerLoginPage, true),
-            textfieldUtility.passwordTextfield(passwordControllerLoginPage, true),
+            textfieldUtility.passwordTextfield(
+                passwordControllerLoginPage, true),
             forgotYourPasswordTextButton(),
-            CustomButton(container: ProfilePageUtility.loginSignUpButtonContainer(true, true),onTap: () {/* giriş yapma fonksiyonu*/},text: 'Giriş Yap',),
+            CustomButton(
+              container:
+                  ProfilePageUtility.loginSignUpButtonContainer(true, true),
+              onTap: () {/* giriş yapma fonksiyonu*/},
+              text: 'Giriş Yap',
+            ),
             ProfilePageUtility.lineWithOrText(),
-            CustomButton(container: ProfilePageUtility.loginSignUpButtonContainer(true, false),onTap: () => Get.to(() => const ParticipantSignUpPage()),text: 'Kaydol',),
-            const SizedBox(height: 58,),
+            CustomButton(
+              container:
+                  ProfilePageUtility.loginSignUpButtonContainer(true, false),
+              onTap: () => Get.to(() => const ParticipantSignUpPage()),
+              text: 'Kaydol',
+            ),
+            const SizedBox(
+              height: 58,
+            ),
           ]),
     );
   }
@@ -36,14 +50,19 @@ class ParticipantLoginPage extends StatelessWidget {
         padding: const EdgeInsets.fromLTRB(227, 0, 24, 40),
         child: TextButton(
             onPressed: () {/* Şifreyi unutma durumundaki fonksiyon gelecek*/},
-            child: const Text('Şifreni mi unuttun?',style: TextStyle(color: AppColors.meteorite),
+            child: const Text(
+              'Şifreni mi unuttun?',
+              style: TextStyle(color: AppColors.meteorite),
             )));
   }
 
   Padding loginPageTitle() {
     return const Padding(
       padding: EdgeInsets.fromLTRB(0, 140, 0, 85),
-      child: Text('Giriş Yap', style: AppTextStyles.loginSignUpBigTitle,),
+      child: Text(
+        'Giriş Yap',
+        style: AppTextStyles.loginSignUpBigTitle,
+      ),
     );
   }
 }

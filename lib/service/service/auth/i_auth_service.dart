a84889f96947e67cb522bfb3e6976f146e9091ef
Behzod Faiziev <behzod.faiziev@gmail.com>
2023-03-10 15:23:59 +0300

    updates!

--- lib/service/service/auth/i_auth_service.dart
+++ lib/service/service/auth/i_auth_service.dart
@@ -4,8 +4,11 @@ import '../../model/common/login/login_model.dart';
 import '../../model/common/signup/sign_up_model.dart';
 
 abstract class IAuthService {
-
   Future<UserCredential?> signInWithEmail(LoginModel loginModel);
 
-  Future<UserCredential?> signUpWithEmail(SignUpModel signUpModel);
+  ///Password is passed outside model to make sure that password is not saved to db
+  Future<UserCredential?> signUpWithEmail({
+    required SignUpModel signUpModel,
+    required String password,
+  });
 }

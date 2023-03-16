7ec59f9edcd21cfe14e1b2a3bdae4a1a6b747daf
Behzod Faiziev <behzod.faiziev@gmail.com>
2023-03-16 23:41:34 +0300

    updated firebase manager

---
+++ lib/core/managers/firebase/firestore/models/created_id_response.g.dart
@@ -0,0 +1,17 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'created_id_response.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+CreatedIdResponse _$CreatedIdResponseFromJson(Map<String, dynamic> json) =>
+    CreatedIdResponse(
+      id: json['id'] as String?,
+    );
+
+Map<String, dynamic> _$CreatedIdResponseToJson(CreatedIdResponse instance) =>
+    <String, dynamic>{
+      'id': instance.id,
+    };

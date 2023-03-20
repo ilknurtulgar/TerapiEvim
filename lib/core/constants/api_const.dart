ce092e394d854ceb807d0c4ae63a004257654ad7
Behzod Faiziev <behzod.faiziev@gmail.com>
2023-03-20 10:19:46 +0300

    corrected activities const

--- lib/core/constants/api_const.dart
+++ lib/core/constants/api_const.dart
@@ -1,6 +1,6 @@
 class APIConst {
   static const String baseUrl = '';
   static const String users = 'users';
-  static const String activities = 'users';
+  static const String activities = 'activities';
   static const String therapist = 'therapist';
 }

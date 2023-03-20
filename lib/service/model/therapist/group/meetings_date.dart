49adc2bfc37746aa089a2f0885da9ce54a9280c1
Behzod Faiziev <behzod.faiziev@gmail.com>
2023-03-20 11:36:28 +0300

    updated meetings model

--- lib/service/model/therapist/group/meetings_date.dart
+++ lib/service/model/therapist/group/meetings_date.dart
@@ -1,5 +1,5 @@
-import 'package:vexana/vexana.dart';
 import 'package:json_annotation/json_annotation.dart';
+import '../../../../core/managers/firebase/firestore/interface/i_network_model.dart';
 part 'meetings_date.g.dart';
 
 @JsonSerializable()

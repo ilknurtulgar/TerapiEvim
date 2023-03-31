548205c4af9e349b9a4dbc3cc807eba44eb5adbb
Behzod Faiziev <behzod.faiziev@gmail.com>
2023-03-31 12:31:25 +0300

    updated firestore manager

--- lib/core/managers/firebase/firestore/firestore_manager.dart
+++ lib/core/managers/firebase/firestore/firestore_manager.dart
@@ -83,6 +83,76 @@ class FirestoreManager<E extends INetworkModel<E>?>
   }
 
   @override
+  Future<IResponseModel<R?, E?>> read<T extends INetworkModel<T>, R>(
+      {required T parseModel,
+      required String collectionPath,
+      String? docId,
+      int limit = AppConst.tenItemsPerPage,
+      String? collectionPath2}) async {
+    QuerySnapshot? response;
+    try {
+      /// return paginated items
+      if (collectionPath2 != null) {
+        response = await _database
+            .collection(collectionPath)
+            .doc(docId)
+            .collection(collectionPath2)
+            .limit(limit)
+            .get();
+      } else {
+        response =
+            await _database.collection(collectionPath).limit(limit).get();
+      }
+
+      final List<QueryDocumentSnapshot<Object?>> queryList = response.docs;
+
+      return _getResponseResult<T, R>(queryList, parseModel);
+    } catch (e) {
+      await crashlyticsManager.sendACrash(
+          error: e.toString(), stackTrace: StackTrace.current, reason: '');
+      return _onError(e);
+    }
+  }
+
+  @override
+  Future<IResponseModel<R?, E?>> readWhere<T extends INetworkModel<T>, R>(
+      {required T parseModel,
+      required String collectionPath,
+      required String whereField,
+      required Object whereIsEqualTo,
+      String? docId,
+      int limit = AppConst.tenItemsPerPage,
+      String? collectionPath2}) async {
+    QuerySnapshot? response;
+    try {
+      /// return paginated items
+      if (collectionPath2 != null) {
+        response = await _database
+            .collection(collectionPath)
+            .doc(docId)
+            .collection(collectionPath2)
+            .where(whereField, isEqualTo: whereIsEqualTo)
+            .limit(limit)
+            .get();
+      } else {
+        response = await _database
+            .collection(collectionPath)
+            .where(whereField, isEqualTo: whereIsEqualTo)
+            .limit(limit)
+            .get();
+      }
+
+      final List<QueryDocumentSnapshot<Object?>> queryList = response.docs;
+
+      return _getResponseResult<T, R>(queryList, parseModel);
+    } catch (e) {
+      await crashlyticsManager.sendACrash(
+          error: e.toString(), stackTrace: StackTrace.current, reason: '');
+      return _onError(e);
+    }
+  }
+
+  @override
   Future<IResponseModel<R?, E?>> readOne<T extends INetworkModel<T>, R>({
     required T parseModel,
     required String collectionPath,

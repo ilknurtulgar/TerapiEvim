4b0ae2f798c1641f224c1f986f51cbde74ba5ef7
Behzod Faiziev <behzod.faiziev@gmail.com>
2023-04-09 00:23:43 +0300

    confirmation service

---
+++ lib/model/therapist/confirmation/t_confirmation_model.dart
@@ -0,0 +1,32 @@
+import 'package:json_annotation/json_annotation.dart';
+
+import '../../../../core/managers/firebase/firestore/interface/i_network_model.dart';
+
+part 't_confirmation_model.g.dart';
+
+@JsonSerializable()
+class ConfirmationModel extends INetworkModel<ConfirmationModel> {
+  String? id, therapistId, uploadedPdfUrl, isConfirmed;
+
+  /// pickedPdfPath is required
+  @JsonKey(includeToJson: false, includeFromJson: false)
+  String? pickedPdfPath;
+
+  ConfirmationModel({
+    this.id,
+    this.therapistId,
+    this.uploadedPdfUrl,
+    this.isConfirmed,
+    this.pickedPdfPath,
+  });
+
+  @override
+  ConfirmationModel fromJson(Map<String, dynamic> json) =>
+      ConfirmationModel.fromJson(json);
+
+  factory ConfirmationModel.fromJson(Map<String, dynamic> json) =>
+      _$ConfirmationModelFromJson(json);
+
+  @override
+  Map<String, dynamic>? toJson() => _$ConfirmationModelToJson(this);
+}

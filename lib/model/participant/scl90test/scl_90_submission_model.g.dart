69b2b08df792616cb86efa825196f209be7a0572
Behzod Faiziev <behzod.faiziev@gmail.com>
2023-04-09 14:53:23 +0300

    updates

---
+++ lib/model/participant/scl90test/scl_90_submission_model.g.dart
@@ -0,0 +1,19 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'scl_90_submission_model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+Scl90SubmissionModel _$Scl90SubmissionModelFromJson(
+        Map<String, dynamic> json) =>
+    Scl90SubmissionModel(
+      isScl90Submitted: json['isScl90Submitted'] as bool?,
+    );
+
+Map<String, dynamic> _$Scl90SubmissionModelToJson(
+        Scl90SubmissionModel instance) =>
+    <String, dynamic>{
+      'isScl90Submitted': instance.isScl90Submitted,
+    };

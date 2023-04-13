e3f7bab37240beabdfab26ba57ecdbaec49f34df
Behzod Faiziev <behzod.faiziev@gmail.com>
2023-04-13 21:47:55 +0300

    updates

---
+++ lib/model/common/session/session_id_model.g.dart
@@ -0,0 +1,17 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'session_id_model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+SessionIdModel _$SessionIdModelFromJson(Map<String, dynamic> json) =>
+    SessionIdModel(
+      sessionId: json['sessionId'] as String?,
+    );
+
+Map<String, dynamic> _$SessionIdModelToJson(SessionIdModel instance) =>
+    <String, dynamic>{
+      'sessionId': instance.sessionId,
+    };

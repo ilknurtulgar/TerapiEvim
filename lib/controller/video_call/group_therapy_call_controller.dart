7f60fd9473fb939fe19201dd79c11086bf715893
GizemGoksu <gizemgks_282@hotmail.com>
2023-04-15 00:21:48 +0300

    update

--- lib/controller/video_call/group_therapy_call_controller.dart
+++ lib/controller/video_call/group_therapy_call_controller.dart
@@ -13,22 +13,32 @@ class PGroupCallController extends GetxController {
 
   var shareAuthority = false.obs;
   var openAllMics = false.obs;
+  var hasSecondTherapistControl =
+      TherapistTabControle.SecondTherapistHasNotControl.obs;
 
-  void therapistSwitchButtonFunction(bool isToOpenMics, bool value) {
+  void therapistSwitchButtonFunction(bool isToOpenMics, bool value,{List<PersonInCallModel>? participants}) {
     if (isToOpenMics) {
-      openAllMics.value = value;
+        openAllMics.value = value;
+        if(value) participants!.forEach((element) {element.isMicOn.value=true;});
+        else participants!.forEach((element) {element.isMicOn.value=false;});
     } else {
       shareAuthority.value = value;
+        if (value) hasSecondTherapistControl.value = TherapistTabControle.SecondTherapistHasControl;
+        else hasSecondTherapistControl.value = TherapistTabControle.SecondTherapistHasNotControl;
     }
   }
 
-  void openTherapistTab(List<PersonInCallModel> participants) {
+  void openTherapistTab(List<PersonInCallModel> participants,
+      Rx<TherapistTabControle> whoHasControl) {
     Get.snackbar('', '',
         padding: EdgeInsets.zero,
         borderRadius: 0,
         margin: EdgeInsets.zero,
         backgroundColor: AppColors.mineShaft,
-        titleText: TherapistTab(participants: participants),
+        titleText: TherapistTab(
+          participants: participants,
+          whoHasControl: whoHasControl,
+        ),
         messageText: const SizedBox.shrink(),
         snackPosition: SnackPosition.BOTTOM,
         duration: const Duration(minutes: 1));

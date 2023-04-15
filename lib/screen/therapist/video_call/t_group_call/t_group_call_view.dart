8151f927bbd380a7f1d19e27f43059fbd05d4b89
Behzod Faiziev <behzod.faiziev@gmail.com>
2023-04-15 22:53:31 +0300

    updated TGroupCallView

--- lib/screen/therapist/video_call/t_group_call/t_group_call_view.dart
+++ lib/screen/therapist/video_call/t_group_call/t_group_call_view.dart
@@ -3,8 +3,6 @@ import 'package:get/get.dart';
 import '../../../../../core/extension/context_extension.dart';
 import '../../../../controller/therapist/video_call/t_group_call_controller.dart';
 import '../../../../core/base/component/video_call/buttons/video_call_buttons.dart';
-import '../../../../core/base/component/video_call/tab/therapist_tab.dart';
-import '../../../../core/base/ui_models/video_call/person_in_call_model.dart';
 import '../../../../core/base/util/base_utility.dart';
 import '../../../../core/base/util/text_utility.dart';
 import '../../../../core/base/view/base_view.dart';
@@ -95,17 +93,17 @@ class TGroupCallView extends StatelessWidget {
     );
   }
 
-  void openTherapistTab(List<PersonInCallModel> participants) {
-    Get.snackbar('', '',
-        padding: EdgeInsets.zero,
-        borderRadius: 0,
-        margin: EdgeInsets.zero,
-        backgroundColor: AppColors.mineShaft,
-        titleText: TherapistTab(participants: participants),
-        messageText: const SizedBox.shrink(),
-        snackPosition: SnackPosition.BOTTOM,
-        duration: const Duration(minutes: 1));
-  }
+  // void openTherapistTab(List<PersonInCallModel> participants) {
+  //   Get.snackbar('', '',
+  //       padding: EdgeInsets.zero,
+  //       borderRadius: 0,
+  //       margin: EdgeInsets.zero,
+  //       backgroundColor: AppColors.mineShaft,
+  //       titleText: TherapistTab(participants: participants),
+  //       messageText: const SizedBox.shrink(),
+  //       snackPosition: SnackPosition.BOTTOM,
+  //       duration: const Duration(minutes: 1));
+  // }
 
   void sendIsolatedCall(String name) {
     Get.dialog(AlertDialog(

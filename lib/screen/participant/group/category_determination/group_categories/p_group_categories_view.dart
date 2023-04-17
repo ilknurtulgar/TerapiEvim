4d841c7c0973bca4b53aebae2e431df12c930384
jjasminn <jasminn247@outlook.com>
2023-04-17 20:52:50 +0300

    psuh

--- lib/screen/participant/group/category_determination/group_categories/p_group_categories_view.dart
+++ lib/screen/participant/group/category_determination/group_categories/p_group_categories_view.dart
@@ -1,53 +1,56 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:terapievim/controller/participant/group/p_group_category_controller.dart';
 import 'package:terapievim/core/base/component/group/group_info_container.dart';
 import 'package:terapievim/core/base/view/base_view.dart';
+import 'package:terapievim/model/therapist/group/t_group_model.dart';
 import '../../../../../core/base/component/app_bar/my_app_bar.dart';
+import '../../../../../core/base/ui_models/card_model.dart';
 import '../../../../../core/base/util/base_utility.dart';
 import '../../../../../core/base/util/text_utility.dart';
 
-class PGroupCategoriesView extends StatefulWidget {
+class PGroupCategoriesView extends StatelessWidget {
   const PGroupCategoriesView({super.key});
 
-  @override
-  State<PGroupCategoriesView> createState() => _PGroupCategoriesViewState();
-}
-
-class _PGroupCategoriesViewState extends State<PGroupCategoriesView> {
   @override
   Widget build(BuildContext context) {
     return BaseView<PGroupCategoryController>(
       getController: PGroupCategoryController(),
       onPageBuilder: (context, controller) => Scaffold(
-        appBar: MyAppBar(title: GroupTextUtil.groupCategoriesHeading),
-        body: SafeArea(
-          child: Padding(
-            padding: AppPaddings.pagePadding,
-            child: listOfGroupCategories(controller),
-          ),
-        ),
-      ),
+          appBar: MyAppBar(title: GroupTextUtil.groupCategoriesHeading),
+          body: listOfGroupCategories(controller)),
     );
   }
 
   Widget listOfGroupCategories(PGroupCategoryController controller) {
-    return ListView.builder(
-      shrinkWrap: true,
-      itemCount: DemoInformation.geciciKategoriSayisi,
-      itemBuilder: (context, index) => Padding(
-        padding: AppPaddings.componentPadding,
-        child: GroupInformationContainer(
-            onTap: controller.join,
-            groupName: DemoInformation.tmpGroupName,
-            mainTherapist: DemoInformation.tmpMainTherapist,
-            secondTherapist: DemoInformation.tmpMainTherapist,
-            numberOfParticipant: DemoInformation.tmpParticipantNumber,
-            numberOfWeek: DemoInformation.tmpCount,
-            numberOfSession: DemoInformation.tmpSessionNumber),
+    return Obx(
+      () => ListView.builder(
+        padding: AppPaddings.pagePadding,
+        itemCount: controller.groups.length,
+        itemBuilder: (context, index) {
+          final TGroupModel group = controller.groups[index];
+          return Padding(
+            padding: AppPaddings.componentPadding,
+            child: controller.groups.isEmpty
+                ? Text("Grup yok")
+                : GroupInformationContainer(
+                    onTap: controller.join,
+                    groupName: group.name!,
+                    mainTherapist: therapists(DemoInformation.imagePath,
+                        DemoInformation.therapistName),
+                    secondTherapist: therapists(DemoInformation.imagePath,
+                        DemoInformation.therapistName2),
+                    numberOfParticipant: group.participantsId?.length ?? 0,
+                    numberOfWeek: DemoInformation.tmpCount,
+                    numberOfSession: DemoInformation.tmpSessionNumber),
+          );
+        },
       ),
     );
   }
-}
 
+  CardModel therapists(String imagePath, String name) =>
+      CardModel(imagePath: imagePath, title: name);
+}
 
 //burdan datalari halledecem

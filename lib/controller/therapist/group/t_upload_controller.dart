6f1b3190dc1c25bdd75be27f5865ed5540d1cbe1
Behzod Faiziev <behzod.faiziev@gmail.com>
2023-04-17 21:58:12 +0300

    updates

--- lib/controller/therapist/group/t_upload_controller.dart
+++ lib/controller/therapist/group/t_upload_controller.dart
@@ -1,3 +1,23 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-class TUploadController extends GetxController {}
+import '../../base/base_controller_2.dart';
+import '../therapist_controller.dart';
+
+class TUploadController extends BaseController2 {
+  @override
+  void setContext(BuildContext context) {}
+
+  @override
+  void onInit() {
+    therapistController = Get.find();
+
+    isUploaded = therapistController.isDiplomaUploaded;
+
+    super.onInit();
+  }
+
+  late TherapistController therapistController;
+
+  RxBool isUploaded = false.obs;
+}

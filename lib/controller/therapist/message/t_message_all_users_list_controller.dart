b32dc933fca542af2b7caaf1814562d5925408fa
ilknurtulgar <ilknur001-@hotmail.com>
2023-04-22 10:37:10 +0300

    update

--- lib/controller/therapist/message/t_message_all_users_list_controller.dart
+++ lib/controller/therapist/message/t_message_all_users_list_controller.dart
@@ -1,9 +1,11 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:get/get.dart';
 import 'package:terapievim/controller/base/base_controller.dart';
 
 import '../../../core/base/util/base_utility.dart';
 import '../../../model/common/user/user_model.dart';
+import '../../../model/therapist/group/t_group_model.dart';
 import '../../../service/message/i_message_service.dart';
 import '../../../service/message/message_service.dart';
 
@@ -16,9 +18,21 @@ class TMessageAllUsersListController extends GetxController
   @override
   Future<void> onInit() async {
     firestoreDBService = MessageService(vexaFireManager.networkManager);
-    final groups = await firestoreDBService.getGroupsOrdered();
+    final List<TGroupModel> groups =
+        await firestoreDBService.getGroupsOrdered();
+
+    for (TGroupModel group in groups) {
+      groupIds.add(group.id!);
+      groupUsers[group.id!] =
+          await firestoreDBService.getAllUsersByGroupId(group.id!);
+    }
+
     super.onInit();
   }
+
+  List<String> groupIds = [];
+
+  RxMap<String, List<UserModel>> groupUsers = <String, List<UserModel>>{}.obs;
 
   late List<RxBool> isOpen =
       List.generate(DemoInformation.groupList.length, (index) => false.obs);
@@ -29,6 +43,17 @@ class TMessageAllUsersListController extends GetxController
   }
 
   late IMessageService firestoreDBService;
+  List<String> data = [];
+  void coll() {
+    FirebaseFirestore.instance
+        .collection('messagegroups')
+        .get()
+        .then((QuerySnapshot querySnapshot) {
+      querySnapshot.docs.forEach((doc) {
+        data.add(doc['messagegroups']);
+      });
+    });
+  }
 
   Future<List<UserModel>> getAllUser() async {
     var allUserList = await firestoreDBService.getAllUsersByGroupId('');
